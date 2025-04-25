b1e30d11f8c40565ecbf04f3ac6df96138f31778
sameershaikh126878 <ywallah6@gmail.com>
2025-04-25 22:56:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-api_id = "api id"
-api_hash = "api hadh"
+api_id = "25051520"
+api_hash = "91c0e86e56bb2f711454d86779091033"
 bot_token = "bot token"
-auth_users = [7890781002]
+auth_users = [5500536251]
 sudo_users = [7890781002]
