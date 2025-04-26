574deb11002047962400d08eec0523d8bf9d963a
decorb <decorb18@gmail.com>
2025-04-26 17:08:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ api_id = "17950436"
 api_hash = "9d8ae4d35b1106df910b94dcaf73d346"
 bot_token = "7647388277:AAFwAQBjQvks_7uwZFkAc0NXrmBSv-yLNCA"
 auth_users = [7615363859]
-sudo_users = [7615363859]
+sudo_users = []
