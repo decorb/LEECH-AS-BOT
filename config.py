4124e57c49ea73eddceefdf9fad53eeeb4604e6a
sameershaikh126878 <ywallah6@gmail.com>
2025-04-26 15:14:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ api_id = "25051520"
 api_hash = "91c0e86e56bb2f711454d86779091033"
 bot_token = "7271958152:AAFOf7DWtlwtZ5QOop1qpQcTkZsmGi-jnyQ"
 auth_users = [5500536251]
-sudo_users = [7890781002]
+sudo_users = [7615363859]
